5e04c6b8ae7a86bea145f3100c846910b6caf41b
SithijiVihagana <sithijivihagana52@.com>
2025-11-26 17:56:50 +0530

    Category based search

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,42 @@
-let input = document.getElementById("txt_mealsearch");
-let heading = document.getElementById("meal_result_header");
-let btn_search = document.getElementById("btn_search");
-input.addEventListener("keypress", e => {
-    if (e.key === "Enter") {
-        e.preventDefault();  // prevents form reload
-        let meal_name = input.value.trim();
-        apiCall(meal_name);
+document.addEventListener("DOMContentLoaded", function () {
+
+    let input = document.getElementById("txt_mealsearch");
+    let heading = document.getElementById("meal_result_header");
+    let btn_search = document.getElementById("btn_search");
+
+    if (input) {
+        input.addEventListener("keypress", e => {
+            if (e.key === "Enter") {
+                e.preventDefault();
+                apiCall(input.value.trim());
+            }
+        });
     }
-});
 
-async function apiCall(meal_name) {
+    if (btn_search) {
+        btn_search.addEventListener("click", e => {
+            e.preventDefault();
+            apiCall(input.value.trim());
+        });
+    }
 
-    try {
-        let res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${meal_name}`);
-        let data = await res.json();
-        
-        if (data.meals === null) {
-            heading.innerText = "No results found!!";
-        } else {
-            heading.innerText = "Search Result for " + (meal_name);
-            displayMeals(data.meals);
+    async function apiCall(meal_name) {
+        try {
+            let res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${meal_name}`);
+            let data = await res.json();
+
+            if (data.meals === null) {
+                heading.innerText = "No results found!!";
+            } else {
+                heading.innerText = "Search Result for " + meal_name;
+                displayMeals(data.meals);
+            }
+        } catch (error) {
+            console.error("Fetch Error:", error);
         }
-    } catch (error) {
-        console.error("Fetch Error:", error);
     }
 
-}
-
-btn_search.addEventListener("click", e => {
-    e.preventDefault();
-    let meal_name = input.value.trim();
-    apiCall(meal_name);
-});
-
-function displayMeals(meals) {
+    function displayMeals(meals) {
     let container = document.getElementById("searchResults");
     container.innerHTML = "";
 
@@ -53,6 +56,60 @@ function displayMeals(meals) {
         container.innerHTML += result;
     });
 }
+
+    
+
+    let category_heading = document.getElementById("category_header");
+    let category_input = document.getElementById("txt_categorysearch");
+    let btn_category = document.getElementById("btn_category");
+
+        if (category_input) {
+            category_input.addEventListener("keypress", e => {
+                if (e.key === "Enter") {
+                    categorySearch(category_input.value.trim());
+                }
+            });
+        }
+
+        if (btn_category) {
+            btn_category.addEventListener("click", e => {
+                categorySearch(category_input.value.trim());
+            });
+        }
+
+
+    async function categorySearch(category_name) {
+        try {
+            let res = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category_name}`);
+            let content = await res.json();
+
+            if (content.meals === null) {
+                category_heading.innerText = "No results found!!";
+            } else {
+                category_heading.innerText = "Search Result for " + category_name;
+
+                let container = document.getElementById("Results_category");
+                container.innerHTML = "";
+
+                content.meals.forEach(meal => {
+                    container.innerHTML += `
+                    <div class="col-md-3 mb-4">
+                        <div class="card h-100 shadow-sm meal-card" onclick="openMeal(${meal.idMeal})">
+                            <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}">
+                            <div class="card-body">
+                                <h5 class="card-title">${meal.strMeal}</h5>
+                            </div>
+                        </div>
+                    </div>`;
+                });
+            }
+        } catch (error) {
+            console.error("Fetch Error:", error);
+        }
+    }
+
+});
+
 
 function openMeal(id) {
     window.location.href = `meal.html?id=${id}`;
@@ -75,7 +132,7 @@ async function random() {
         }
 
         let container = document.getElementById("Results");
-        container.innerHTML = ""; 
+        container.innerHTML = "";
         let card = `
             <div class="col-md-6 mb-4 mx-auto">
 
@@ -95,7 +152,7 @@ async function random() {
             </div>
         `;
 
-        container.innerHTML = card; 
+        container.innerHTML = card;
 
     } catch (error) {
         console.error("Fetch Error:", error);
